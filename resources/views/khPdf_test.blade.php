d2540705790feb62fea7625324b87f80a6d990de
Yoeurn-Yat <Yoeurnyat@gmail.com>
2026-09-08 23:04:53 +0700

    install kh-pdf library

---
+++ resources/views/khPdf_test.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        p {
+            font-size: 25px;
+            /* font-family: 'battambang';
+            font-weight: bold; */
+
+            font-family: 'kantumruy_pro';
+        }
+    </style>
+</head>
+
+<body>
+    <p>សួស្តី ពិភពលោក ! Hello World</p>
+    <p><b>សួស្តី ពិភពលោក ! Hello World (Bold)</b></p>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use KhmerPdf\LaravelKhPdf\Facades\PdfKh;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/kh-pdf-test', function () {
+    $html = view('khPdf_test', ['title' => 'សួស្តី ពិភពលោក!'])->render();
+    return PdfKh::loadHtml($html)->stream('khmer_document.pdf');
 });
